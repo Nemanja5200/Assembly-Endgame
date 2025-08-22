3f82b0a7273008039c55b513980aa72c7e64badb
Nemanja5200 <nemanja.bogojevic@shindiristudio.com>
2025-08-22 11:53:42 +0200

    Letters

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+
 import { Header } from "./components/Header.jsx";
 import { Notice } from "./components/Notice.jsx";
 import { Lives } from "./components/Lives.jsx";
+import {Letters} from "./components/Letters.jsx";
+import "./css/App.css";
 
 function App() {
   return (
     <>
-      <Header />
-      <Notice />
-      <Lives />
+        <div className="app-container">
+            <Header />
+            <Notice />
+            <Lives />
+            <Letters/>
+        </div>
+
     </>
   );
 }
---
+++ src/components/Letters.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../css/Letters.css";
+export const Letters = () => {
+    return (
+        <>
+            <div className="letters-container">
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+        </>
+    );
+};
